6b987f0b6deb9d167d2ef60aff7a5ecd18949482
Sir Cathy W <everbloom@gmail.com>
2026-02-24 21:46:34 +1100

    Add puzzle9 and puzzle10 to the 15x15 puzzle collection

--- src/puzzles/15x15/index.ts
+++ src/puzzles/15x15/index.ts
@@ -6,9 +6,11 @@ import puzzle5 from './puzzle5.json';
 import puzzle6 from './puzzle6.json';
 import puzzle7 from './puzzle7.json';
 import puzzle8 from './puzzle8.json';
+import puzzle9 from './puzzle9.json';
+import puzzle10 from './puzzle10.json';
 
 import { PuzzleDefinition } from '../../types/nonogram';
 
-const puzzles = [puzzle1, puzzle2, puzzle3, puzzle4, puzzle5, puzzle6, puzzle7, puzzle8] as PuzzleDefinition[];
+const puzzles = [puzzle1, puzzle2, puzzle3, puzzle4, puzzle5, puzzle6, puzzle7, puzzle8, puzzle9, puzzle10] as PuzzleDefinition[];
 
 export default puzzles.sort((a, b) => a.difficulty - b.difficulty);
