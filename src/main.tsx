0d650d86dea10f4a747b28dc1e6655b7e32e3c86
Sir Cathy W <everbloom@gmail.com>
2026-02-24 20:59:17 +1100

    Fix bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { enableMapSet } from 'immer'
 import './index.css'
 import App from './App.tsx'
 import { applyTheme, applyCellSize, subscribeToSystemTheme } from './themeStorage'
 
+enableMapSet()
 applyTheme()
 applyCellSize()
 subscribeToSystemTheme()
